d765d8eacc3e2f84c2c6b026266e1ac6aada0a6a
Tom <t.p.kellett@gmail.com>
2021-12-01 13:58:00 +0000

    attempting further configuration of CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://newsforce-api.herokuapp.com'
+    origins '*'
 
     resource '/index',
       headers: :any,
