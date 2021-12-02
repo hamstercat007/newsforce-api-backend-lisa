d055e3d3277677fd47ae6352a60e8417d4562303
Arthur Fincham <arthurfincham@me.com>
2021-12-02 13:51:11 +0000

    add spec for tagging

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,10 @@
 class StoriesController < ApplicationController
+  include StoriesHelper
   def index
     @stories = Story.all.order(publish_date: :desc)
+    @stories.map do |story|
+      story.tag_list = StoriesHelper.add_to_tag_list(story)
+    end
     render json: @stories
   end
 end
