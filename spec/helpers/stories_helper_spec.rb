b05af998a25a1168237f02b60e5e769ef22a6db2
Arthur Fincham <arthurfincham@me.com>
2021-12-02 14:30:43 +0000

    refactor with Rufo

--- spec/helpers/stories_helper_spec.rb
+++ spec/helpers/stories_helper_spec.rb
@@ -1,27 +1,26 @@
-require './app/helpers/stories_helper.rb'
+require "./app/helpers/stories_helper.rb"
 require "json"
 
 describe StoriesHelper do
-  
   file = File.open "spec/mocks/sample_data.json"
   data = JSON.load file
 
-  it 'creates a tag list ' do
+  it "creates a tag list " do
     mock = data[0]
-    expect(StoriesHelper.add_to_tag_list(mock)).to eq(['us-canada', 'bbc-news'])
+    expect(StoriesHelper.add_to_tag_list(mock)).to eq(["us-canada", "bbc-news"])
   end
 
-  it 'creates a tag list ' do
+  it "creates a tag list " do
     mock = data[1]
-    expect(StoriesHelper.add_to_tag_list(mock)).to eq(['al-jazeera', 'asia'])
+    expect(StoriesHelper.add_to_tag_list(mock)).to eq(["al-jazeera", "asia"])
   end
 
-  it 'creates a tag list ' do
+  it "creates a tag list " do
     mock = data[2]
-    expect(StoriesHelper.add_to_tag_list(mock)).to eq(['al-jazeera'])
+    expect(StoriesHelper.add_to_tag_list(mock)).to eq(["al-jazeera"])
   end
 
-  it 'creates a tag list ' do
+  it "creates a tag list " do
     mock = data[7]
     expect(StoriesHelper.add_to_tag_list(mock)).to eq(["associated-press", "Paris"])
   end
