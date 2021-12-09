d3660e82b879d9ac1d9c00eda8c77095eee8954e
hamstercat007 <lisamac421@gmail.com>
2021-12-09 13:49:53 +0000

    removed completed

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,11 +1,10 @@
 class StoriesController < ApplicationController
   include StoriesHelper
   def index
-    @pagy, @stories = pagy(Story.all.order(publish_date: :desc))
+    @stories = Story.all.order(publish_date: :desc)
     @stories.map do |story|
       story.tag_list = StoriesHelper.add_to_tag_list(story)
     end
-    render json: { data: @stories,
-                   pagy: pagy_metadata(@pagy) }
+    render json: @stories
   end
 end
